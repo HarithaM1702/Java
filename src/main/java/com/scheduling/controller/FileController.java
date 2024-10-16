955621236416f801b74607c30d89467311296259
HarithaM1702 <harithakrishna0217@gmail.com>
2024-10-16 15:11:40 +0530

    meeee

--- src/main/java/com/scheduling/controller/FileController.java
+++ src/main/java/com/scheduling/controller/FileController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.scheduling.service.FileGeneratorService;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/home")
 public class FileController {
 
     @Autowired
